2b9289485cd1a612949142fb274dcf02b9cea066
QuimbanoPrime <qaz027@pm.me>
2026-01-18 21:34:44 -0800

    insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(nums):
+    j = 1
+    for i in range(len(nums)):
+        j = i
+        while j > 0 and nums[j-1] > nums[j]:
+            temp = nums[j]
+            nums[j] = nums[j-1]
+            nums[j-1] = temp
+            j -= 1
+
+    return nums
