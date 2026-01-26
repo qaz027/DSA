9a5d815f7f00ef6f1dd8b2e0cf3255eecc06eadb
QuimbanoPrime <qaz027@pm.me>
2026-01-25 22:55:03 -0800

    more linked lists

--- linked_lists.py
+++ linked_lists.py
@@ -10,3 +10,32 @@ class Node:
 
     def __repr__(self):
         return self.val
+
+# adding tail
+
+class LinkedList:
+    def add_to_tail(self, node):
+        if not self.head:
+            self.head = node
+        else:
+            last = None
+            for nodes in self.__iter__():
+                last = nodes
+            last.next = node
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(node.val)
+        return " -> ".join(nodes)
